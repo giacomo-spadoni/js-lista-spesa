12ca2a3d3904aff25e4e23cb4ae3da5a09094097
giacomo-spadoni <giaky.spado@gmail.com>
2024-04-28 13:36:11 +0200

    M2 and M3 complited

--- js/main.js
+++ js/main.js
@@ -3,9 +3,20 @@ const spesa = ['pasta', 'burro', 'nutella', 'pane', 'marmellata']
 
 // utilizzando il ciclo for
 
-// for (let i = 0; i < spesa.length; i++) {
-//     const element = spesa[i];
-//     const myLi = document.createElement('li')
-//     myLi.textContent = '- ' + element
-//     list.append(myLi)
-// }
+/*for (let i = 0; i < spesa.length; i++) {
+    const element = spesa[i];
+    const myLi = document.createElement('li')
+    myLi.textContent = '- ' + element
+    list.append(myLi)
+}*/
+
+// utilizzo il ciclo while
+
+let i = 0
+while (i < spesa.length) {
+    const element = spesa[i];
+    const myLi = document.createElement('li')
+    myLi.textContent = '- ' + element
+    list.append(myLi)
+    i++
+}

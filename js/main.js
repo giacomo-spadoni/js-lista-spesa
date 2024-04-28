0e7f69e88d556ad9d7412522b242287e7d08eb77
giacomo-spadoni <giaky.spado@gmail.com>
2024-04-28 13:33:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const list = document.getElementById('lista-spesa')
+const spesa = ['pasta', 'burro', 'nutella', 'pane', 'marmellata']
+
+// utilizzando il ciclo for
+
+// for (let i = 0; i < spesa.length; i++) {
+//     const element = spesa[i];
+//     const myLi = document.createElement('li')
+//     myLi.textContent = '- ' + element
+//     list.append(myLi)
+// }
